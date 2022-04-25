208bf008165e3e3af828657c26c8df1ed0b4b6dd
Taro Serigano <taroserigano@gmail.com>
2022-04-25 09:41:56 -0500

    Update traping_rainwater.js

--- traping_rainwater.js
+++ traping_rainwater.js
@@ -11,13 +11,22 @@ const elevationArray = [0, 1, 0, 2, 1, 0, 3, 1, 0, 1, 2]
 
 const getTrappedRainwater = function(heights) {
 
-  let left = 0, right = heights.length - 1, totalWater = 0, maxLeft = 0, maxRight = 0;
+  let left = 0, 
+      // very RIGHT END 
+      right = heights.length - 1, 
+      
+      totalWater = 0, maxLeft = 0, maxRight = 0;
   
+  // run from the LEFT to RIGHT  end 
   while(left < right) {
+    // if this point lower than the RIGHT END 
     if(heights[left] <= heights[right]) {
+      // if this point, higher than the current MAX LEFT 
       if(heights[left] >= maxLeft) { 
+        // MAX LEFT is this point
         maxLeft = heights[left]
       } else { 
+        // 
         totalWater += maxLeft - heights[left];
       }
       left++;
@@ -37,3 +46,47 @@ const getTrappedRainwater = function(heights) {
 
 
 console.log(getTrappedRainwater(elevationArray));
+
+
+
+----------------------------------------------------------------------
+
+L= 0, R = height-1
+maxL = 0, maxR = 0 
+
+
+-Run from Left to Right
+	IF: L point <= R point?
+		IF: L point >= maxL ?
+			maxLeft is now = L point
+		ELSE:
+			TotalWater += maxLeft - L point
+			
+			Left++ 
+			
+	ELSE: 
+		IF: R point >= maxRight?	
+			maxRight = R point 
+		ELSE: 
+			TotalWater += maxRight - R point 
+		
+			Right--
+			
+- Return TotalWater			
+			
+			
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
