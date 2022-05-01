9769c4b9833b94b9754ab79220ece54ea31c133f
Taro Serigano <taroserigano@gmail.com>
2022-04-30 20:26:01 -0500

    Create reverse_linked.js

---
+++ reverse_linkedlist/reverse_linked.js
@@ -0,0 +1,60 @@
+   var head; // head of list
+     class Node {
+        constructor(val) {
+            this.data = val;
+            this.next = null;
+        }
+    }
+ 
+    function reverse(head) {
+        if (head == null || head.next == null)
+            return head;
+
+        /*
+         * reverse the rest list and put the first element at the end
+         */
+        while(head.next){
+        var rest = reverse(head.next);
+
+        head.next.next = head;
+ 
+        /* tricky step -- see the diagram */
+        head.next = null;
+ 
+        return rest;
+        }
+
+    }
+ 
+    /* Function to print linked list */
+    function print() {
+    var temp = head;
+        while (temp != null) {
+            document.write(temp.data + " ");
+            temp = temp.next;
+        }
+        document.write();
+    }
+ 
+    function push(data) {
+    var temp = new Node(data);
+        temp.next = head;
+        head = temp;
+    }
+ 
+    /* Driver program to test above function */
+     
+        /* Start with the empty list */
+ 
+        push(4);
+        push(3);
+        push(2);
+        push(1);
+ 
+        document.write("Given linked list<br/>");
+        print();
+ 
+        head = reverse(head);
+ 
+        document.write("<br/>Reversed Linked list<br/>");
+        print();
