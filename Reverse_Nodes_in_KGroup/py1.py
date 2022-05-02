f8888e75af1bdeb807e8c5334cf6f78d52385e5c
Taro Serigano <taroserigano@gmail.com>
2022-05-02 11:57:24 -0500

    Update py1.py

--- Reverse_Nodes_in_KGroup/py1.py
+++ Reverse_Nodes_in_KGroup/py1.py
@@ -4,35 +4,37 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        
-        dummy = ListNode(0, head) 
-        groupPrev = dummy 
-        
-        while True: 
-            kth = self.getKth(groupPrev, k)
-            if not kth: 
-                break 
-            groupNext = kth.next 
-            print("grrrr",groupNext)
-            
-            prev, curr = kth.next, groupPrev.next 
-            
-            while curr != groupNext:
-                tmp = curr.next
-                curr.next = prev 
-                prev = curr
-                curr = tmp 
-            
-            tmp = groupPrev.next 
-            groupPrev.next = kth 
-            groupPrev = tmp 
-        return dummy.next    
-            
-              
-    def getKth(self, curr, k):
-        while curr and k > 0:
-            curr = curr.next
-            k -= 1
-        print(curr)    
-        return curr    
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:\
+
+        def reverse(head, k):
+            p = head
+            for i in range(k):
+                if(p):
+                    p = p.next
+                else:
+                    return head
+
+            pre = head 
+            now = head.next
+            print("Now " , now)
+            pre.next = None
+            print("PRE ", pre)
+            i = 1
+            while(now != None and i < k):
+                nex = now.next
+                print(" next " , nex)
+                now.next = pre
+                pre = now 
+                now = nex
+                i += 1
+            print("THE whole pre " , pre)
+            print(" head " , head)
+            if(now != None):
+                head.next = reverse(now, k)
+
+            return pre
+
+        if(head):
+            return reverse(head, k)
+        else:
+            return None
