b1836bb6cc4a6c4d51ffec9b06308c0947a1a0ef
Taro Serigano <taroserigano@gmail.com>
2022-05-02 19:20:33 -0500

    Create min1.py

---
+++ Min_Window_Substring_H/min1.py
@@ -0,0 +1,24 @@
+# time O(n) 
+Space O(k) k as number of different characters 
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        count, remain = collections.Counter(t), len(t)
+        print(count)
+        i, left, right = 0, -1, -1
+        for j, c in enumerate(s):
+            remain -= count[c] > 0
+            count[c] -= 1
+            if remain:
+                continue
+            while count[s[i]] < 0:  # greedily discard uneeds
+                count[s[i]] += 1
+                i += 1
+            if right == -1 or j-i+1 < right-left+1:
+                left, right = i, j
+        return s[left:right+1]
