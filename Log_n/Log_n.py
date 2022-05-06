967db44fa0f4051ba04719cbbb76a43f1d94f5ea
Taro Serigano <taroserigano@gmail.com>
2022-05-06 18:31:47 -0500

    Create Log_n.py

---
+++ Log_n/Log_n.py
@@ -0,0 +1,24 @@
+O(Log n):
+  
+  n = 8 
+  
+  2 ^3 = 8 
+  
+  log 2 8 = 3 
+  
+  function log(n) { 
+    n = Math.floor(n / 2)
+    return log(n(
+      } 
+      
+      
+      
+
+
+
+
+
+
+
+
+
