69318846d24c42a8f52b485d425f6a42109a63b4
Taro Serigano <taroserigano@gmail.com>
2022-05-17 09:09:44 -0500

    Update Clone_Graph.py

--- MEDIUM/Clone_Graph.py
+++ MEDIUM/Clone_Graph.py
@@ -1,26 +1,34 @@
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val = 0, neighbors = None):
-        self.val = val
-        self.neighbors = neighbors if neighbors is not None else []
-"""
-
 class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
-        oldToNew = {} 
+        map = {} 
         
         def dfs(node):
-            if node in oldToNew:
-                return oldToNew[node]
+            if node in map:
+                # print(map[node])
+                return map[node]
             
             copy = Node(node.val) 
-            oldToNew[node] = copy
-            for nei in node.neighbors: 
+            # print(node.val, " " , node.neighbors)
+            map[node] = copy   # 1:[ , ]
+            
+            for nei in node.neighbors: # usually the node has TWO neighbors [2, 4] 
+                print(nei.val)
+                # add to the copy 
                 copy.neighbors.append(dfs(nei))
+                # case 1. if node already in map, return this NODE
+                # case 2. "for nei in node.neighbors:" releae two neighbors of this NODE, and add to the copy
+                
             return copy 
+        
         return dfs(node) if node else None  
-    
-    
-    
+
+# seenMap:
+#
+#     1: [2,4] --->   2: [3,1]
+#        ^               ^
+#        |               |
+#        v               v
+#     4: [1,3] --->   3: [2,4]
+
+        
         
