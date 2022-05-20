e5feef6d999e6038c015325b9f1bdf42c9177f81
Taro Serigano <taroserigano@gmail.com>
2022-05-20 13:37:26 -0500

    Create product-of-array-except-self.py

---
+++ MEDIUM/product-of-array-except-self.py
@@ -0,0 +1,16 @@
+class Solution(object):
+def productExceptSelf(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: List[int]
+    """
+    answer = [1]*len(nums)
+    answer[0] = 1
+    for i in range(1, len(nums)):
+        answer[i] = answer[i-1] * nums[i-1]
+    rightProduct = 1
+    for i in range(len(nums)-1, -1, -1):
+        answer[i] = answer[i] * rightProduct
+        rightProduct *= nums[i]
+    return answer
+    
