96a328aab28fd2a9d2bc025593f9e8d58bd78093
Taro Serigano <taroserigano@gmail.com>
2022-06-24 14:15:13 -0500

    Update Container with Most Water.py

--- MEDIUM/Container with Most Water.py
+++ MEDIUM/Container with Most Water.py
@@ -4,8 +4,31 @@ res = 0
 l, r = 0, len(height) -1 
 
 while l < r:
-area = (r - l) * min(height[l], height[r]) 
-res = max(res, area) 
+# area = width * minimum Height  
+  area = (r - l) * min(height[l], height[r])
+# update result   
+  res = max(res, area) 
+  
+# Move   
+  if height[l] < height[r]: 
+    l += 1 
+  else: 
+    r -=1 
+    
+return res 
+
+
+
+
+
+
+
+
+
+# 8, 2, 3, 4, 6, 8, 7 
+
+# min height would be 7 for maximum area 
+
 
 
 
